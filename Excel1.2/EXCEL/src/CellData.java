faa8c5728c63a913ae540e8c3506ad5de32946dc
jarguetag77 <47362155+jarguetag77@users.noreply.github.com>
2019-05-20 23:09:59 -0600

    jkhl
    kuhlk

---
+++ Excel1.2/EXCEL/src/CellData.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.Color;
+import java.io.Serializable;
+/**
+ *
+ * @author user
+ */
+class CellData {
+    public String s = new String("");
+  
+  public String toString() { return this.s; }
+  
+  protected Object clone() throws CloneNotSupportedException { return super.clone(); }
+}
+    
+
